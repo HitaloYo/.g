b2d8f3b18a5a46d20be85c101f721971268676f7
HitaloYo <76579977+HitaloYo@users.noreply.github.com>
2023-12-04 16:56:34 -0300

    Add files via upload

--- DJANGO/note_taking/notes/models.py
+++ DJANGO/note_taking/notes/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Note(models.Model):
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    description = models.CharField(max_length=256) 
+    done = models.BooleanField(default=False)
--- DJANGO/note_taking/notes/views.py
+++ DJANGO/note_taking/notes/views.py
@@ -4,3 +4,6 @@ from django.template import loader
 def index(request):
     template = loader.get_template("index.html")
     return HttpResponse(template.render({}, request))
+def index2(request):
+    template = loader.get_template("index2.html")
+    return HttpResponse(template.render({}, request))
